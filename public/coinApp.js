7fb42820f37b4a2d626bdecf779c78a89f412181
robfera <robb.feraru@gmail.com>
2018-04-03 15:36:31 +0200

    You can now change the currency

--- public/coinApp.js
+++ public/coinApp.js
@@ -10,21 +10,31 @@ $(document).ready(function () {
     // Config
 
     // Arrays of all the currencies and symbols
-    const currencyArray = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"]
-    const currencySymbolArray = ["$", "R$", "$", "Fr. ", "$", "¥", "Kč", "kr. ", "€", "£", "$", "Ft ", "Rp ", "₪", "₹", "¥", "₩", "$", "RM", "kr ", "$", "₱", "₨ ", "zł", "₽", "kr ", "S$", "฿", "₺", "NT$", "R "];
+    const currencyArray = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "USD", "ZAR"]
+    const currencySymbolArray = ["$", "R$", "$", "Fr. ", "$", "¥", "Kč", "kr. ", "€", "£", "$", "Ft ", "Rp ", "₪", "₹", "¥", "₩", "$", "RM", "kr ", "$", "₱", "₨ ", "zł", "₽", "kr ", "S$", "฿", "₺", "NT$", "$", "R "];
 
     // The default currency that is already selected
     const currencyDefault = currencyArray[8];
 
     // The chosen currency
-    const currency = currencyDefault
+    let currency = currencyDefault
 
     // Get symbol based on the selected currency
-    const currencySymbol = currencySymbolArray[currencyArray.indexOf(currency)];
+    let currencySymbol = currencySymbolArray[currencyArray.indexOf(currency)];
 
     // Get a string based on the selected currency, so you can call the correct price data
-    const currencyObject = "price_" + currency.toLowerCase();
+    let currencyObject = "price_" + currency.toLowerCase();
     
+
+    // Display the currencies
+    for (let i = 0, currencyLength = currencyArray.length; i < currencyLength; i++) {
+        $("#currency-" + (i + 1)).append("<a class=\"currencyChangeButton\">" + currencyArray[i] + "</a>");
+    }
+    $(".data-currency-display").append(currency);
+
+
+
+
     // JSON API
     let jsonApi = "https://api.coinmarketcap.com/v1/ticker/?convert=" + currency + "&limit=200";
 
@@ -90,73 +100,6 @@ $(document).ready(function () {
         // Take the coin's icon based on his symbol and display symbol by adding the class. https://github.com/AllienWorks/cryptocoins
         $(".cc").addClass(coinSymbol);
 
-
-        // UPDATE DATA REAL-TIME
-        setInterval(function () {
-
-            $.getJSON(jsonApi, function (apiData) {
-
-                // Get new data
-                coinData = apiData[index];
-
-                // Get the coin's price
-                NEWcoinDataPrice = parseFloat(coinData[currencyObject].replace(/\"/g, ""));
-                if (NEWcoinDataPrice > 10) {
-                    NEWcoinDataPrice = NEWcoinDataPrice.toFixed(2).replace(".", ",");
-                } else if (NEWcoinDataPrice > 1 && NEWcoinDataPrice < 10) {
-                    NEWcoinDataPrice = NEWcoinDataPrice.toFixed(3).replace(".", ",");
-                } else {
-                    NEWcoinDataPrice = NEWcoinDataPrice.toFixed(5).replace(".", ",");
-                }
-
-                // Get percent change
-                NEWcoinDataPercentChange = coinData.percent_change_24h.replace(/\"/g, "").replace(".", ",");
-
-
-                // Display data
-
-                //Coin price
-                if (NEWcoinDataPrice !== coinDataPrice) {
-
-                    // Color effects when the price changes
-                    if (NEWcoinDataPrice > coinDataPrice) {
-                        $("#coin-price").css("color", "#10ff00");
-                        $("#coin-price").animate({ color: "#ffffff" }, 5900);
-                        var priceColorChangePositive = setInterval(function () {
-                            $("#coin-price").stop();
-                        }, 5900);
-                        clearInterval(priceColorChangePositive);
-                    } else {
-                        $("#coin-price").css("color", "#ff0000");
-                        $("#coin-price").animate({ color: "#ffffff" }, 5900);
-                        var priceColorChangeNegative = setInterval(function () {
-                            $("#coin-price").stop();
-                        }, 5900);
-                        clearInterval(priceColorChangeNegative);
-                    }
-                    $("#coin-price").html(NEWcoinDataPrice);
-                    coinDataPrice = NEWcoinDataPrice;
-                }
-
-                //Percent change
-                if (NEWcoinDataPercentChange !== coinDataPercentChange) {
-                    $("#percent-number").html(NEWcoinDataPercentChange);
-                    coinDataPercentChange = NEWcoinDataPercentChange;
-                }
-
-                // Color text in green if the percentage is positive, and in red if the percentage is negative
-                if (coinDataPercentChange.replace(/,/g, '.') > 0) {
-                    $("#percent").addClass("positive-percent");
-                } else {
-                    $("#percent").addClass("negative-percent");
-                }
-                
-                console.log(NEWcoinDataPrice);
-
-            });
-
-        }, 6000);
-
         // Color text in green if the percentage is positive, and in red if the percentage is negative
         if (coinDataPercentChange.replace(/,/g, '.') > 0) {
             $("#percent").addClass("positive-percent");
@@ -164,6 +107,144 @@ $(document).ready(function () {
             $("#percent").addClass("negative-percent");
         }
 
+    });
+
+
+    // UPDATE DATA REAL-TIME
+    let updateData = function() {
+
+        $.getJSON(jsonApi, function (apiData) {
+
+            // Variables
+            let index = 0;
+            let coinData;
+            let NEWcoinDataPrice;
+            let NEWcoinDataPercentChange;
+            let coinDataPrice = $("#coin-price").html();
+            let coinDataPercentChange = $("#percent-number").html();
+
+            // Check for coin based by id in the url
+            for (let apiDataLength = apiData.length; index < apiDataLength; index++) {
+                if (apiData[index].id == coinId) {
+                    coinData = apiData[index];
+                    break;
+                };
+            };
+            
+            // Get the coin's price
+            NEWcoinDataPrice = parseFloat(coinData[currencyObject].replace(/\"/g, ""));
+            if (NEWcoinDataPrice > 10) {
+                NEWcoinDataPrice = NEWcoinDataPrice.toFixed(2).replace(".", ",");
+            } else if (NEWcoinDataPrice > 1 && NEWcoinDataPrice < 10) {
+                NEWcoinDataPrice = NEWcoinDataPrice.toFixed(3).replace(".", ",");
+            } else {
+                NEWcoinDataPrice = NEWcoinDataPrice.toFixed(5).replace(".", ",");
+            }
+
+            // Get percent change
+            NEWcoinDataPercentChange = coinData.percent_change_24h.replace(/\"/g, "").replace(".", ",");
+
+
+            // Display data
+
+            //Coin price
+            if (NEWcoinDataPrice !== coinDataPrice) {
+
+                // Color effects when the price changes
+                if (NEWcoinDataPrice > coinDataPrice) {
+                    $("#coin-price").css("color", "#10ff00");
+                    $("#coin-price").animate({ color: "#ffffff" }, 5900);
+                    var priceColorChangePositive = setInterval(function () {
+                        $("#coin-price").stop();
+                    }, 5900);
+                    clearInterval(priceColorChangePositive);
+                } else {
+                    $("#coin-price").css("color", "#ff0000");
+                    $("#coin-price").animate({ color: "#ffffff" }, 5900);
+                    var priceColorChangeNegative = setInterval(function () {
+                        $("#coin-price").stop();
+                    }, 5900);
+                    clearInterval(priceColorChangeNegative);
+                }
+                $("#coin-price").html(NEWcoinDataPrice);
+                coinDataPrice = NEWcoinDataPrice;
+            }
+
+            //Percent change
+            if (NEWcoinDataPercentChange !== coinDataPercentChange) {
+                $("#percent-number").html(NEWcoinDataPercentChange);
+                coinDataPercentChange = NEWcoinDataPercentChange;
+            }
+
+            // Color text in green if the percentage is positive, and in red if the percentage is negative
+            if (coinDataPercentChange.replace(/,/g, '.') > 0) {
+                $("#percent").addClass("positive-percent");
+            } else {
+                $("#percent").addClass("negative-percent");
+            }
+            
+            console.log(NEWcoinDataPrice);
+
+        });
+
+    };
+
+    // Start real time update
+    let realTime = setInterval(updateData, 6000);
+
+    // Change currency
+    $(".currencyChangeButton").click(function (data) {
+        let selectedCurrency = data.currentTarget.text;
+        currency = selectedCurrency;
+        currencySymbol = currencySymbolArray[currencyArray.indexOf(currency)];
+        currencyObject = "price_" + currency.toLowerCase();
+        $(".data-currency-display").empty().append(currency);
+
+        // Get new data
+
+
+        // JSON API
+        jsonApi = "https://api.coinmarketcap.com/v1/ticker/?convert=" + currency + "&limit=200";
+
+        $.getJSON(jsonApi, function (apiData) {
+        
+            // Variables
+            let index2 = 0;
+            let coinData;
+            let coinDataPrice;
+
+            // Check for coin based by id in the url
+            for (let apiDataLength = apiData.length; index2 < apiDataLength; index2++) {
+                if (apiData[index2].id == coinId) {
+                    coinData = apiData[index2];
+                    break;
+                };
+            };
+
+            // Get the coin's price
+            coinDataPrice = parseFloat(coinData[currencyObject].replace(/\"/g, ""));
+            if (coinDataPrice > 10) {
+                coinDataPrice = coinDataPrice.toFixed(2).replace(".", ",");
+            } else if (coinDataPrice > 1 && coinDataPrice < 10) {
+                coinDataPrice = coinDataPrice.toFixed(3).replace(".", ",");
+            } else {
+                coinDataPrice = coinDataPrice.toFixed(5).replace(".", ",");
+            }
+
+
+            // Display Data
+            
+            //Currency symbol
+            $("#currency-symbol").html(currencySymbol);
+            //Coin price
+            $("#coin-price").html(coinDataPrice);
+            //Currency
+            $("#currency").html(currency);
+        
+        // Restart real time update
+            clearInterval(realTime);
+        });
+        realTime = setInterval(updateData, 6000);
     });
 
 });
--- public/homepageApp.js
+++ public/homepageApp.js
@@ -7,21 +7,54 @@ $(document).ready(function () {
 
     // Config
 
-    // Arrays of all the currencies and symbols
-    const currencyArray = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"]
-    const currencySymbolArray = ["$", "R$", "$", "Fr. ", "$", "¥", "Kč", "kr. ", "€", "£", "$", "Ft ", "Rp ", "₪", "₹", "¥", "₩", "$", "RM", "kr ", "$", "₱", "₨ ", "zł", "₽", "kr ", "S$", "฿", "₺", "NT$", "R "];
+    // Arrays of all the currencies and symbolss
+    const currencyArray = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "USD", "ZAR"]
+    const currencySymbolArray = ["$", "R$", "$", "Fr. ", "$", "¥", "Kč", "kr. ", "€", "£", "$", "Ft ", "Rp ", "₪", "₹", "¥", "₩", "$", "RM", "kr ", "$", "₱", "₨ ", "zł", "₽", "kr ", "S$", "฿", "₺", "NT$", "$", "R "];
 
     // The default currency that is already selected
     const currencyDefault = currencyArray[8];
 
     // The chosen currency
-    const currency = currencyDefault
+    let currency = currencyDefault
 
     // Get symbol based on the selected currency
-    const currencySymbol = currencySymbolArray[currencyArray.indexOf(currency)];
+    let currencySymbol = currencySymbolArray[currencyArray.indexOf(currency)];
 
     // Get a string based on the selected currency, so you can call the correct price data
-    const currencyObject = "price_" + currency.toLowerCase();
+    let currencyObject = "price_" + currency.toLowerCase();
+
+    // Display the currencies
+    for (let i = 0, currencyLength = currencyArray.length; i < currencyLength; i++) {
+        $("#currency-" + (i + 1)).append("<a class=\"currencyChangeButton\">" + currencyArray[i] + "</a>");
+    }
+    $(".data-currency-display").append(currency);
+
+    // Change currency
+    $(".currencyChangeButton").click(function (data) {
+    
+        let selectedCurrency = data.currentTarget.text;
+        currency = selectedCurrency;
+        currencySymbol = currencySymbolArray[currencyArray.indexOf(currency)];
+        currencyObject = "price_" + currency.toLowerCase();
+        $(".data-currency-display").empty().append(currency);
+
+
+        // Get new data
+
+        jsonApi = "https://api.coinmarketcap.com/v1/ticker/?convert=" + currency + "&limit=200";
+
+        // Update the Top list
+        $.getJSON(jsonApi, function (topCoins) {
+
+            for (let i = 0, topCoinsLength = topCoins.length; i < topCoinsLength; i++) {
+                $("#topcoin-" + (i + 1)).empty().append("<b>" + (i + 1) + ".</b> <b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[i].id + "\" >" + topCoins[i].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[i][currencyObject]).toFixed(2));
+            }
+
+        });
+
+
+    });
+
 
 
 
@@ -30,22 +63,15 @@ $(document).ready(function () {
 
     // Get the coins from Node and display the top 10 in order, assigning them the equivalent url. Example: BITCOIN = http://127.0.0.1:8080/coins/bitcoin
     $.getJSON(jsonApi, function (topCoins) {
-        console.log(topCoins);
-        $("#topcoin-1").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[0].id + "\" >" + topCoins[0].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[0][currencyObject]).toFixed(2));
-        $("#topcoin-2").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[1].id + "\" >" + topCoins[1].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[1][currencyObject]).toFixed(2));
-        $("#topcoin-3").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[2].id + "\" >" + topCoins[2].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[2][currencyObject]).toFixed(2));
-        $("#topcoin-4").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[3].id + "\" >" + topCoins[3].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[3][currencyObject]).toFixed(2));
-        $("#topcoin-5").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[4].id + "\" >" + topCoins[4].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[4][currencyObject]).toFixed(2));
-        $("#topcoin-6").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[5].id + "\" >" + topCoins[5].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[5][currencyObject]).toFixed(2));
-        $("#topcoin-7").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[6].id + "\" >" + topCoins[6].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[6][currencyObject]).toFixed(2));
-        $("#topcoin-8").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[7].id + "\" >" + topCoins[7].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[7][currencyObject]).toFixed(2));
-        $("#topcoin-9").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[8].id + "\" >" + topCoins[8].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[8][currencyObject]).toFixed(2));
-        $("#topcoin-10").append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[9].id + "\" >" + topCoins[9].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[9][currencyObject]).toFixed(2));
+
+        for (let i = 0, topCoinsLength = topCoins.length; i < topCoinsLength; i++) {
+            $("#topcoin-" + (i + 1)).append("<b class=\"topcoin-name\">" + "<a class=\"effect-underline\" href=\"/coins/" + topCoins[i].id + "\" >" + topCoins[i].name + "</b>" + "<a/>" + ": " + currencySymbol + parseFloat(topCoins[i][currencyObject]).toFixed(2));
+        }
+
     });
 
     // Get all the coins from Node
     $.getJSON(jsonApi, function (coinMarketCap) {
-        console.log(coinMarketCap);
         // When the form is submitted
         $(".main-form").submit(function (event) {
             let startTime = performance.now();
@@ -62,11 +88,11 @@ $(document).ready(function () {
                      if (coinMarketCap[i].name == coin) {
                         window.location.assign(website + "/coins/" + coinMarketCap[i].id);
                         return coinMarketCap[i].id; */
-                     if (coinMarketCap[i].symbol == coin.toUpperCase()) {
+                    if (coinMarketCap[i].symbol == coin.toUpperCase()) {
                         window.location.assign(website + "/coins/" + coinMarketCap[i].id);
                         return coinMarketCap[i].id;
 
-                    // Search engine
+                        // Search engine
                     } else {
                         if (coinMarketCap[i].id.search(coin.toLowerCase()) !== -1) {
                             foundCoins.push(coinMarketCap[i].id);
@@ -78,22 +104,22 @@ $(document).ready(function () {
                 if (foundCoins.length == 1) {
                     window.location.assign(website + "/coins/" + foundCoins[0]);
                     return coinMarketCap[i].id;
-                
-                // If more coins were found, show the results
+
+                    // If more coins were found, show the results
                 } else if (foundCoins.length > 1) {
-                    
+
                     $(".coins-table").hide();
                     $("#search").css("visibility", "visible");
                     $("#search-info").css("visibility", "visible");
 
-                    for(var i = 0; i < foundCoins.length; i++) {
+                    for (var i = 0; i < foundCoins.length; i++) {
                         $("#search-results").append("<p class=\"text-info\">" + "<a class=\"effect-underline\" href=\"/coins/" + foundCoins[i] + "\" >" + foundCoins[i] + "</a></p>");
                     }
 
                     $("#search-info-text").html((foundCoins.length) + " coins found (" + ((performance.now() - startTime) / 1000).toFixed(4) + " seconds)");
                     return foundCoins;
 
-                // If no coins were found, throw the error message
+                    // If no coins were found, throw the error message
                 } else {
                     $(".error-message").css("visibility", "visible");
                     return null;
@@ -102,7 +128,6 @@ $(document).ready(function () {
 
             // Fire the checkCoin function, and store the coin in a variable called "coin"
             let coin = checkCoin($("#coin").val());
-            console.log(coin);
         });
     });
 });
