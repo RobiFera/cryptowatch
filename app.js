8df127c31bc57e1032067cf8d5b1bf0daac35e76
robfera <robb.feraru@gmail.com>
2018-04-06 16:45:17 +0200

    Developed the admin login page

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const http = require("http").Server(app);
+const helmet = require("helmet");
 
 // Config 
 const port = 8080;
+let username = "admin";
+let password = "password";
 
 // BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,7 +18,8 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/cryptoicons'));
 
-
+// Helmet
+app.use(helmet());
 
 // -- ROUTES --
 
@@ -27,9 +31,20 @@ app.get("/", function (req, res) {
 
 // Coin page
 app.get("/coins/:coinId", function (req, res) {
+    res.sendFile(__dirname + "/public/coin.html");  
+});
 
-    res.sendFile(__dirname + "/public/coin.html")
-    
+// Admin panel
+app.get("/admin", function (req, res) {
+    res.sendFile(__dirname + "/public/admin.html");
+});
+
+app.post("/admin", function(req, res) {
+    if (req.body.username == "admin" && req.body.password == "password") {
+        console.log("CORRECT");
+    } else {
+        console.log("NOT CORRECT");
+    };
 });
 
 // Redirect non-existing pages to homepage
